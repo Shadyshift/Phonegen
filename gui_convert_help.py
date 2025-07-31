96b050b2425c466a2810843155d902a3ab89ac36
Shadyshift <schakenboscas@gmail.com>
2025-07-31 17:54:12 +0200

    Add files via upload

---
+++ gui_convert_help.py
@@ -0,0 +1,3 @@
+print("The GUI version of convert makes it more easy to select files")
+print("to get in the gui version you can type convert -gui")
+print("You can convert files in the JSON, CSV, AND TXT file extention")
